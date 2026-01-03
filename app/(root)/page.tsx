7841dcef0f60b56063161f8dbcef5d5e9c150155
Prateeksha <prateeksharg26@gmail.com>
2026-01-03 21:40:15 +0530

    implement header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div className="flex min-h-screen home-wrapper">
+           Home
+        </div>
+    );
+}
